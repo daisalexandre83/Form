2b7365bef45caa5c2744983a3de30ea98c6ae60e
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2021-10-04 16:50:06 -0300

    Inserção do HTML,classes no CSS e funções no Javascript.

--- script.js
+++ script.js
@@ -5,47 +5,65 @@ let B7Validator = {
 
         let inputs = form.querySelectorAll('input');
     
-    for (let i=0; i < inputs.length; i++) {
-        let input = inputs[i];
-         let check = B7Validator.checkInput(input);
-         if (check !== true) {
-             send = false;
-             console.log(check);
-         }
+        B7Validator.clearErrors();
 
-    }
-    if (send) {
-        form.submit();
-     }
-    },
-    checkInput:(input)=>{
-        let rules = input.getAttribute('data-rules');
-        if (rules !== null) {
-           rules = rules.split('|');
-           for (let k in rules) {
-              let rDetails = rules[k].split('=');
-            switch (rDetails[0]) {
-                case 'required':
-                    if (input.value == '') {
-                        return 'Campo não pode ser vazio';
-                    }
-                    break;
-            
-                case 'min':
-                    break;
+        for (let i=0; i < inputs.length; i++) {
+            let input = inputs[i];
+            let check = B7Validator.checkInput(input);
+            if (check !== true) {
+                send = false;
+                B7Validator.showError(input,check);
+
             }
-               
-           }
-
-        }
-        return true;
     }
+        if (send) {
+            form.submit();
+        }
+        },
+        checkInput:(input)=>{
+            let rules = input.getAttribute('data-rules');
+            if (rules !== null) {
+            rules = rules.split('|');
+            for (let k in rules) {
+                let rDetails = rules[k].split('=');
+                switch (rDetails[0]) {
+                    case 'required':
+                        if (input.value == '') {
+                            return 'Campo não pode ser vazio';
+                        }
+                        break;
+                
+                    case 'min':
+                        break;
+                }
+                
+            }
+
+            }
+            return true;
+        },
+        showError:(input,error)=>{
+            input.style.borderColor = "#ff0000";
+
+            let errorElement = document.createElement('div');
+            errorElement.classList.add('error');
+            errorElement.innerHTML = error;
+
+            input.parentElement.insertBefore(errorElement,input.ElementSibling);
+        },
+        clearErrors:()=>{
+            let inputs = form.querySelectorAll('input');
+            for (let i = 0; i < inputs.length; i++) {
+               inputs[i].style='';
+            }
+
+            let errorElements = document.querySelectorAll('.error');
+            for (let i = 0; i < errorElements.length; i++) {
+              errorElements[i].remove();
+            }
+        }
     
  };
     
-
-
-
-
 let form = document.querySelector('.b7validator');
 form.addEventListener('submit',B7Validator.handleSumit)
